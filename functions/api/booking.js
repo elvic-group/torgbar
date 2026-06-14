f0bffcad17cd382f1fe0f58147da304bf9cbff4a
Elvic Kongoloe <elvickongolo@gmail.com>
2026-06-14 12:34:18 +0200

    test: minimal echo CF function

--- functions/api/booking.js
+++ functions/api/booking.js
@@ -1,28 +1,14 @@
 export async function onRequestPost(context) {
-  try {
-    const body = await context.request.text();
-    const response = await fetch("http://187.124.42.2:8090/api/collections/bookings/records", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body,
-    });
-
-    const data = await response.text();
-    return new Response(data, {
-      status: response.status,
+  const body = await context.request.text();
+  return new Response(
+    JSON.stringify({ received: true, body: JSON.parse(body).contact_name }),
+    {
       headers: {
         "Content-Type": "application/json",
         "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "POST, OPTIONS",
-        "Access-Control-Allow-Headers": "Content-Type",
       },
-    });
-  } catch (err) {
-    return new Response(JSON.stringify({ message: "Server error" }), {
-      status: 502,
-      headers: { "Content-Type": "application/json" },
-    });
-  }
+    },
+  );
 }
 
 export async function onRequestOptions() {
